b9e2215c98c4aa079b85e2e4da2f1066c582c139
Stefan Mueller <stefan.mueller@gesis.org>
2020-12-03 15:34:33 +0100

    functions to list attributes

---
+++ R/z11_list_100m_attributes.R
@@ -0,0 +1,25 @@
+#' List 1 hectare attributes of the German Census 2011
+#'
+#' This function lists all attributes of the German Census 2011 with a raster
+#' resolution of 1 hectare. These data were the last that have been published.
+#' While they comprise more attributes than the 1 km^2 ones, the data were
+#' prepared in a less easy to be used format. Note that the returned vector
+#' of attribute names includes the attribute names as they were defined by the
+#' Census folks plus a pattern denoting the code of the attribute in the
+#' original dumped CSV file.
+#'
+#' @return Character vector#'
+#'
+#' @importFrom magrittr %>%
+#'
+#' @export
+z11_list_100m_attributes <- function() {
+  system.file("extdata", package = "z11") %>%
+    paste0("/100m/") %>%
+    list.files() %>%
+    sub(".rds", "", .) %>%
+    setdiff(c("Gitter_ID_100m_x_y", "INSGESAMT_0"))
+}
+
+
+
--- R/z11_list_1km_attributes.R
+++ R/z11_list_1km_attributes.R
@@ -1,3 +1,16 @@
+#' List 1km^2 attributes of the German Census 2011
+#'
+#' This function lists all attributes of the German Census 2011 with a raster
+#' resolution of 1km^2. These data were the first that have been published. And
+#' while they comprise fewer attributes than the 1 hectare ones, the data were
+#' prepared in a way more easy to be used format. Note that the returned vector
+#' of attribute names includes the attribute names as they were defined by the
+#' Census folks.
+#'
+#' @return Character vector#'
+#'
+#' @importFrom magrittr %>%
+#'
 #' @export
 z11_list_1km_attributes <- function() {
   system.file("extdata", "z11_attributes_1km.rds", package = "z11") %>%
@@ -6,5 +19,3 @@ z11_list_1km_attributes <- function() {
     dplyr::select(-Gitter_ID_1km) %>%
     names()
 }
-
-z11_list_1km_attributes()

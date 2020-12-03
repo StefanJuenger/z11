f5976bdc126eea2897218e5a106aa88aa9af6278
Stefan Mueller <stefan.mueller@gesis.org>
2020-12-03 15:34:59 +0100

    internal functions for important and prepping data

---
+++ R/z11_extract_inspire_coordinates.R
@@ -0,0 +1,40 @@
+#' Extract Geocoordinates (EPSG:3035) from INSPIRE IDs
+#'
+#' Retreive centroid geocoordinates for 1km^2 and 1 hectare INSPIRE Grid IDs
+#'
+#' @param inspire_ids Character vector containing INSPIRE IDs
+#' @return tibble
+#'
+#' @importFrom magrittr %>%
+z11_extract_inspire_coordinates <- function (inspire_ids) {
+
+  if (stringr::str_detect(inspire_ids[1], "1km")) {
+    inspire_coordinates <-
+      tibble::tibble(
+        X =
+          substr(inspire_ids, 10, 13) %>%
+          paste0(., "500") %>%
+          as.numeric(),
+        Y =
+          substr(inspire_ids, 5, 8) %>%
+          paste0(., "500") %>%
+          as.numeric()
+      )
+  }
+
+  if (stringr::str_detect(inspire_ids[1], "100m")) {
+    inspire_coordinates <-
+      tibble::tibble(
+        X =
+          substr(inspire_ids, 12, 16) %>%
+          paste0(., "50") %>%
+          as.numeric(),
+        Y =
+          substr(inspire_ids, 6, 10) %>%
+          paste0(., "50") %>%
+          as.numeric()
+      )
+  }
+
+  inspire_coordinates
+}
---
+++ R/z11_wide.R
@@ -0,0 +1,23 @@
+#' Convert German Census 100m X 100m long format files in wide format
+#'
+#' This function expects an R object containing the census data in the long
+#' format. It processes the data as following: \\
+#' 1. Deleting all non-substantial rows from the data (e.g. "INSGESAMT"
+#' rows) \\
+#' 2. Creating identifier for all attribute-code-combinations \\
+#' 3. Transforming of data using the previously created identifier \\
+#' 4. Lastly, all missing values are converted to zeros
+#'
+#' @param x R object containing the census data
+#'
+#' @importFrom magrittr %>%
+z11_wide_100m <- function (x) {
+  x %>%
+    dplyr::arrange(Gitter_ID_100m) %>%
+    dplyr::filter(Merkmal != " INSGESAMT") %>% #, Anzahl_q != 1) %>%
+    dplyr::mutate(merk_code = paste0(Merkmal, "_", Auspraegung_Code)) %>%
+    dplyr::select(Gitter_ID_100m, merk_code, Anzahl) %>%
+    data.table::as.data.table() %>%
+    tidyfast::dt_pivot_wider(names_from = merk_code, values_from = Anzahl)
+    # tidyr::pivot_wider(., names_from = merk_code, values_from = Anzahl)
+}
